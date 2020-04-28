53720c79f5c1bab9512a58466fc277229e163d66
Pavlo Samsonenko <49371242+PavloSamsonenko@users.noreply.github.com>
2020-04-28 16:39:10 +0300

    Лаб. работа №10

---
+++ CSharp_Lab10/CSharp_Lab10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            double xmin, xmax, dx, k, x, z;
+
+            StreamReader file = new StreamReader("D:\\lab.txt");
+            s = file.ReadLine();
+            xmin = Convert.ToDouble(s);
+            Console.WriteLine("xmin={0}", xmin);
+            s = file.ReadLine();
+            xmax = Convert.ToDouble(s);
+            Console.WriteLine("xmax={0}", xmax);
+            s = file.ReadLine();
+            dx = Convert.ToDouble(s);
+            Console.WriteLine("dx={0}", dx);
+            s = file.ReadLine();
+            k = Convert.ToDouble(s);
+            Console.WriteLine("k={0}", k);
+            StreamWriter sw = new StreamWriter("D:\\lab.txt");
+
+            if (xmin + dx < xmax)
+            {
+                for (x = 0; x < xmax; x += dx)
+                {
+                    z = Math.Pow((1 / Math.Tan(k * x)), 1 / 3) + ((Math.Cos(k * x)) / Math.Log(Math.Sin(k * x)));
+                    if (Math.Log(Math.Sin(k * x)) == 0 | Math.Tan(k * x) == 0 | Math.Sin(k * x) <= 0)
+                    {
+                        Console.WriteLine("There is a mistake!");
+                        sw.WriteLine("There is a mistake!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("x = {0} z = {1}", x, z);
+                        sw.WriteLine("x = {0} z = {1}", x, z);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is a mistake!");
+                sw.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
